a18d26f3729a85693f7ef1fcbb89fdc88772a0c1
Malte Kiefer <malte.kiefer@mailbox.org>
2020-01-15 14:44:59 +0100

    update
    Signed-off-by: Malte Kiefer <malte.kiefer@mailbox.org>

--- lxdBackup.sh
+++ lxdBackup.sh
@@ -4,25 +4,34 @@
 ####### FUNCTIONS #######
 #########################
 
-help_message() {
-echo "$(basename "$0") [-h] [-c] -- script to backup and sync lxd container to external/internal repo
 
---- Parameters ---
-    -h  show this help message
-    -c  path to config file (default: ~/.config/lxdbackup/config)"
+usage()
+{
+  echo -e  "$(basename "$0") -- script to backup and sync lxd container to external/internal repo"
+  echo -e  ""
+  echo -e "\t-h --help"
+  echo -e  "\t-h | --help     show this help message"
+  echo -e  "\t-p | --pass     password for gpg encryption"
+  echo -e  "\t-v | --version  print version & third party version"
+  echo -e  ""
+}
+version() {
+  echo -e  "Version: ${VERSION}"
+  echo -e  "GPG Version: $(gpg --version | head -n1 | awk '{print $3}')"
+  echo -e  "RSYNC Version: $(rsync --version | head -n1 | awk '{print $3}')"
 }
 
 #########################
 ######### VARS ##########
 #########################
-#YES/NO => y/n
 
+VERSION=0.0.1
 AUTO_DELETE_OLD_IMAGES=y
 AUTO_DELETE_OLD_ARCHIVES=y
 GPG_ENCRYPTION=y
 LOG_FILE="/var/log/lxdbackup.log"
 LOG_FILE_TIMESTAMP="%m/%d/%Y %H:%M:%S"
-GPGPASS="myStrongPassword"
+GPGPASS=""
 WORKDIR="/tmp/lxdbackup"
 BACKUPDATE=$(date +"%m-%d-%y-%H-%M")
 
@@ -30,21 +39,23 @@ BACKUPDATE=$(date +"%m-%d-%y-%H-%M")
 ######### MAIN ##########
 #########################
 
-while getopts ':hc:' option; do
-  case "$option" in
-    h) help_message
-       exit
-       ;;
-    c) source $OPTARG
-       ;;
-    :) printf "missing argument for -%s\n" "$OPTARG" >&2
-       help_message >&2
-       exit 1
-       ;;
-   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
-       help_message >&2
-       exit 1
-       ;;
-  esac
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -v | --version)
+        version
+        exit
+        ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
 done
-shift $((OPTIND - 1))
